8cc9f16b63e5deb984ceccd7f8c253b37cb102d8
Vinicius Costa <vcdl1997@gmail.com>
2021-09-05 23:24:47 -0300

    Exemplo básico de utilização do pacote react-router-dom

---
+++ src/componentes/BotaoVoltar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './style.css';
+
+const BotaoVoltar = () => {
+    return (
+        <div className="area-botao">
+            <Link className="botao-voltar" to="/">Voltar</Link>
+        </div>
+    );  
+}
+
+export default BotaoVoltar;
